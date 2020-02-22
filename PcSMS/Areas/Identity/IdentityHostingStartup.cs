dbe851a8cf2803e2048d29b73cc67b5b50b4a728
Narayan <narain.guragain@gmail.com>
2020-02-21 22:50:34 -0500

    Project created, identity views added, hidden roles implmented

---
+++ PcSMS/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PcSMS.Data;
+
+[assembly: HostingStartup(typeof(PcSMS.Areas.Identity.IdentityHostingStartup))]
+namespace PcSMS.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ PcSMS/Utility/Who.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PcSMS.Utility
+{
+    public static class Who
+    {
+        public const string Admin = "Admin";
+        public const string Staff = "Staff";
+        public const string Customer = "Customer";
+        public const string AdminAndStaff = "Admin, Staff";
+        public const string All = "Admin, Staff, Customer";
+    }
+}
